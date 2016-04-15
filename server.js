4709bffa6a394dbf6fd899be1c6d706591593a5e
ralex <ralex.rdz@gmail.com>
2016-04-15 10:59:44 -0600

    primer server

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
+server.start(function () {
     console.log('Server running at:', server.info.uri);
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
 });
