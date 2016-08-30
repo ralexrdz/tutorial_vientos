645a56043ad7954622343fd4e6d06d9abe3e9de7
Juan Zinser <juanzinser@Juans-MacBook-Pro-2.local>
2016-08-29 23:56:48 -0500

    clases de login

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -12,13 +12,29 @@ module.exports = function()
     path: '/about',
     handler: function (request, reply) {
         reply.view('about');
-		}
+		}	
 	},
 	{
 	method: 'GET',
     path: '/parameters/{param1}',
     handler: function (request, reply) {
         reply.view('parameters',request.params);
+		}
+	},
+	{
+	method: 'GET',
+    path: '/login/{juan}',
+    handler: function (request, reply) {
+        reply.view('login',request.params);
+
+		}
+	},
+	{
+	method: 'POST',
+    path: '/login_post',
+    handler: function (request, reply) {
+	$('#submit')
+     reply('Ahuevo');
 		}
 	}
 ]}()
